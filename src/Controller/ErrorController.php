b30bfcfd21d9e4fd88200eb89c65687ffc8f41f8
Fabien LIERVILLE <fabien.lierville27@gmail.com>
2026-06-10 09:05:36 +0200

    error

---
+++ src/Controller/ErrorController.php
@@ -0,0 +1,14 @@
+<?php
+namespace src\Controller;
+
+
+class ErrorController extends AbstractController
+{
+    public function show(\Exception $e){
+        return $this->twig->render('error.html.twig',[
+
+            'message' => $e->getMessage()
+        ]);
+    }
+
+}
